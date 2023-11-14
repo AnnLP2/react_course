13bc6a0d767e251831cc5df83199b5633e4fa767
AnnLP2 <ann.vitalina@gmail.com>
2023-11-14 19:38:40 +0200

    lesson 31(react icons)

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,9 @@
-import Alert from "./components/Alert";
-import Button from "./components/Button";
-import { useState } from "react";
-import ListGroup from "./components/ListGroup";
+import { BsCalendarFill } from "react-icons/bs";
 
 function App() {
-  const [alertVisible, setAlertVisibility] = useState(false);
-  let items = ["New York", "Los Angeles", "San Francisco"];
-
-  const handleSelectItem = (item: string) => {
-    console.log(item);
-  };
   return (
     <div>
-      {alertVisible && (
-        <Alert onClose={() => setAlertVisibility(false)}>My alert</Alert>
-      )}
-      <Button color="primary" onClick={() => setAlertVisibility(true)}>
-        My button
-      </Button>
-      <ListGroup
-        items={items}
-        heading="Miami"
-        onSelectItem={handleSelectItem}
-      ></ListGroup>
+      <BsCalendarFill color="red" size="40" />
     </div>
   );
 }
