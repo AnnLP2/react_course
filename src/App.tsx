cbac5da28e2688548f898df448b7e421a0112c39
AnnLP2 <ann.vitalina@gmail.com>
2024-01-04 19:42:49 +0200

    lesson 46 expandable text

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,23 @@
-import { useState } from "react";
+import ExpandableText from "./components/ExpandableText";
 
 function App() {
-  const [game, setGame] = useState({
-    id: 1,
-    player: {
-      name: "John",
-    },
-  });
-
-  const handleClick = () => {
-    setGame({ ...game, player: { ...game.player, name: "Bob" } });
-  };
-
-  return <div></div>;
+  return (
+    <div>
+      <ExpandableText>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quo, animi!
+        Cumque, earum quo ex delectus nihil repellendus pariatur non possimus,
+        accusamus sapiente unde. Quaerat consequatur ut eligendi in, temporibus
+        iure nesciunt vitae maiores suscipit quibusdam tempore. Quisquam maiores
+        vitae molestiae id qui, laborum distinctio. Cumque sunt asperiores
+        eveniet qui a et officiis vel quaerat consequuntur, facilis suscipit
+        maxime numquam ut ab rem reprehenderit quisquam explicabo non autem ea!
+        Molestias accusantium quos alias error quia qui ipsum aliquam deserunt
+        eos sapiente enim consequatur dolorem corrupti, esse id, nostrum a.
+        Veniam obcaecati officia dolorum perspiciatis eveniet ipsam nesciunt
+        iste quisquam. A, aspernatur?
+      </ExpandableText>
+    </div>
+  );
 }
 
 export default App;
